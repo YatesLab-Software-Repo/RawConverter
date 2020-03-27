58550392b73d7f9a40995b4bcb0b1f3da2a63c1d
Titus Jung <titus@integratedproteomics.com>
2020-03-26 17:48:55 -0700

    updating build process

--- RawConverterDotNetCore/build.sh
+++ RawConverterDotNetCore/build.sh
@@ -1,4 +1,5 @@
 dotnet build ./RawConverterDotnetCore.csproj
-cp common/*so bin/release/netcoreapp3.0/
 
- dotnet publish /home/yateslab/gitRepos/RawConverterDotnetCore/RawConverterDotnetCore.csproj /property:GenerateFullPaths=true
+
+dotnet publish --configuration Release ./RawConverterDotnetCore.csproj /property:GenerateFullPaths=true
+#cp Common/*so bin/Release/netcoreapp3.0/
